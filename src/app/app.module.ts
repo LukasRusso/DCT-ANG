3b53e6c886f35249f8a1351e1f2c7ecbe5cca9d5
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2021-11-10 20:14:25 -0300

    alteração do visual do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PadraoPageComponent } from './core/padrao-page/padrao-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PadraoPageComponent
-  ],
+  declarations: [AppComponent, PadraoPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
