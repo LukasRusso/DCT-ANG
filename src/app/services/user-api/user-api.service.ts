6f1431c36f89233cd66257c15c8da9488dd4814b
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2021-11-21 18:00:47 -0300

    criação do usuário com api

---
+++ src/app/services/user-api/user-api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { PostSumary } from 'src/app/site/models/postSumary';
+import { environment } from 'src/environments/environment';
+import { Usuario } from 'src/app/site/models/usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService {
+  url = environment.apiUrl + '/user';
+  constructor(private http: HttpClient) {}
+
+  createUser(user: Usuario): Observable<Usuario> {
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    });
+    let options = { headers: httpHeaders };
+    return this.http.post<Usuario>(this.url, user, options);
+  }
+}
--- src/app/site/pages/subcription-page/subcription-page.component.ts
+++ src/app/site/pages/subcription-page/subcription-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserApiStoreService } from 'src/app/services/user-api/user-api-store.service';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-subscription-page',
@@ -7,7 +10,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./subcription-page.component.css'],
 })
 export class SubscriptionPeageComponent implements OnInit {
-  constructor(private route: Router) {}
+  pageForm: FormGroup;
+  constructor(private route: Router, private fb: FormBuilder, private service: UserApiStoreService) {
+    this.pageForm = this.fb.group({
+      email: ['',Validators.email],
+      senha: ['',Validators.minLength],
+    });
+  }
 
   ngOnInit() {}
+
+  createUser(){
+    if(this.pageForm.valid){
+      this.service.createUser(this.pageForm.value as Usuario).subscribe(() => this.route.navigateByUrl("/login"))
+      this.route.navigateByUrl('/');
+    }else{
+      alert("Preencha o formulario Corretamente")
+    }
+
+  }
 }
